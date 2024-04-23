48b43014a01bb9ee182d10c2164b5782e20e46d9
YASHIKA JAISWAL <111109944+yashikajaiswal31@users.noreply.github.com>
2024-04-23 15:31:23 +0530

    Update Clustering.R

--- Clustering.R
+++ Clustering.R
@@ -1,51 +1,46 @@
-#Installation of required packages
-install.packages("dplyr")
-install.packages("reshape2")
-install.packages("ggthemes")
-install.packages("gganimate")
-install.packages("cluster")
-install.packages("Rtsne")
-install.packages("knitr")
+# Installation of required packages
+install.packages(c("dplyr", "reshape2", "ggthemes", "gganimate", "cluster", "Rtsne", "knitr", "dbscan"))
 
-#Load Libraries
-library(tidyverse)# for data cleaning
+# Load Libraries
+library(tidyverse)     # for data cleaning
 library(dplyr)  
 library(reshape2)
 library(ggthemes)  
 library(gganimate)
-library(cluster)#for cluster analysis 
+library(cluster)       # for cluster analysis 
 library(Rtsne)  
 library(knitr)
-library(Rtsne)
 library(dbscan)
 
-#Import the dataset
-population_data = read.csv("Downloads/world_population.csv")
-#Display structure of the dataset
+# Import the dataset
+population_data <- read.csv("Downloads/world_population.csv")
+
+# Display structure of the dataset
 glimpse(population_data)
 
-#Rename coloumns for clarity
-names(population_data)<-c('Rank','CCA3','Country','Capital','Continent','Population_2022', 'Population_2020', 'Population_2015','Population_2010', 'Population_2000', 'Population_1990', 'Population_1980', 'Population_1970', 'Area', 'Density','Growth_Rate','World_population_perc') 
-#Display first few rows of the dataset
-(head(population_data))
+# Rename columns for clarity
+names(population_data) <- c('Rank', 'CCA3', 'Country', 'Capital', 'Continent', 'Population_2022', 'Population_2020', 'Population_2015', 'Population_2010', 'Population_2000', 'Population_1990', 'Population_1980', 'Population_1970', 'Area', 'Density', 'Growth_Rate', 'World_population_perc') 
+
+# Display first few rows of the dataset
+head(population_data)
 
 # Plot world population percentage by continent using ggplot
-population_data%>% 
-  select(Continent,World_population_perc)%>% 
-  group_by(Continent)%>% 
-  dplyr::summarise(World_population_perc = sum(World_population_perc))%>% 
-  ggplot( aes(x="", y=World_population_perc, fill=Continent)) +
-  geom_bar(stat="identity", width=1, color="white") +
-  coord_polar("y", start=0)+  
-  scale_fill_brewer(palette = "Paired")+
-  theme_void() 
+population_data %>% 
+  select(Continent, World_population_perc) %>% 
+  group_by(Continent) %>% 
+  dplyr::summarise(World_population_perc = sum(World_population_perc)) %>% 
+  ggplot(aes(x = "", y = World_population_perc, fill = Continent)) +
+  geom_bar(stat = "identity", width = 1, color = "white") +
+  coord_polar("y", start = 0) +  
+  scale_fill_brewer(palette = "Paired") +
+  theme_void() +
+  labs(title = "World Population Percentage by Continent") +  # Add heading
+  geom_text(data = population_data %>% slice_head(n = 5), aes(label = Country), hjust = -0.5, size = 3)  # Add labels for the first 5 countries
 
 # Prepare data for clustering
-cluster_data <- population_data%>% 
-  mutate_if(sapply(population_data, is.character), as.factor)%>%
-  select(Country,Area,Density, Growth_Rate,World_population_perc)
-#Display first few rows of cluster_data
-(head(cluster_data,10))
+cluster_data <- population_data %>% 
+  mutate_if(sapply(population_data, is.character), as.factor) %>%
+  select(Country, Area, Density, Growth_Rate, World_population_perc)
 
 # Compute Gower distance matrix
 gower_dist <- daisy(cluster_data[,-1],
@@ -63,13 +58,15 @@ for(i in 2:10){
 sil_width[is.na(sil_width)] <- 0
 
 # Create data frame for silhouette width
-dfs <-data.frame(Cluster_num= 1:10,Silhouette_width=sil_width)
+dfs <- data.frame(Cluster_num = 1:10, Silhouette_width = sil_width)
 
 # Plot silhouette width
-ggplot(dfs, aes(x=Cluster_num,y=Silhouette_width)) + 
-  geom_line()+  
-  scale_x_continuous(breaks=seq(1, 10, 1))+
-  theme_bw()
+ggplot(dfs, aes(x = Cluster_num, y = Silhouette_width)) + 
+  geom_line() +  
+  scale_x_continuous(breaks = seq(1, 10, 1)) +
+  theme_bw() +
+  labs(title = "Silhouette Width for Different Number of Clusters") +  
+  geom_text(data = dfs %>% slice_head(n = 5), aes(label = Cluster_num), hjust = -0.5, size = 3) 
 
 # Perform clustering with k = 3
 pam_fit <- pam(gower_dist, diss = TRUE, k = 3)
@@ -84,8 +81,6 @@ pam_results <- cluster_data %>%
 # Display cluster summary
 pam_results$the_summary
 
-
-set.seed(123)
 # Perform t-SNE dimensionality reduction
 tsne_obj <- Rtsne(gower_dist, is_distance = TRUE)
 
@@ -97,23 +92,25 @@ tsne_data <- tsne_obj$Y %>%
          name = cluster_data$Country)
 
 # Plot distribution of t-SNE clusters
-tsne_data %>% group_by(cluster)%>% count() %>% 
-  ggplot(aes(x=cluster, y = n, fill= cluster)) + 
+tsne_data %>% group_by(cluster) %>% count() %>% 
+  ggplot(aes(x = cluster, y = n, fill = cluster)) + 
   geom_bar(stat='Identity')+  
   coord_flip()+  
   scale_fill_brewer(palette = "Dark2")+
-  theme_bw()
+  theme_bw() +
+  labs(title = "Distribution of t-SNE Clusters")  
 
 # Plot t-SNE visualization with cluster labels
 ggplot(aes(x = X, y = Y), data = tsne_data) +
   geom_point(aes(color = cluster)) +theme(legend.position="none") + 
-  geom_text( 
-    data=tsne_data %>% group_by(cluster) %>%
-      filter(X == max(X) | X==min(X) | Y==max(Y)| Y == min(Y)) %>%
-      arrange(cluster), 
-    aes(label=name),vjust = 0, nudge_y =1
-  ) +scale_color_brewer(palette = "Dark2")+ 
-  theme_bw()
+  geom_text(data = tsne_data %>% group_by(cluster) %>%
+              filter(X == max(X) | X==min(X) | Y==max(Y)| Y == min(Y)) %>%
+              arrange(cluster), 
+            aes(label = name), vjust = 0, nudge_y = 1
+  ) +
+  scale_color_brewer(palette = "Dark2")+ 
+  theme_bw() +
+  labs(title = "t-SNE Visualization with Cluster Labels")  
 
 # Perform hierarchical clustering
 hc_fit <- hclust(gower_dist)
@@ -132,21 +129,22 @@ tsne_data %>%
   geom_bar(stat = 'Identity') +  
   coord_flip() +  
   scale_fill_brewer(palette = "Dark2") +
-  theme_bw()
+  theme_bw() +
+  labs(title = "Distribution of Hierarchical Clusters")  
 
 # Plot the t-SNE visualization with hierarchical cluster labels
 ggplot(aes(x = X, y = Y), data = tsne_data) +
   geom_point(aes(color = hierarchical_cluster)) +
   theme(legend.position = "none") + 
-  geom_text( 
-    data = tsne_data %>% 
-      group_by(hierarchical_cluster) %>% 
-      filter(X == max(X) | X == min(X) | Y == max(Y) | Y == min(Y)) %>% 
-      arrange(hierarchical_cluster), 
-    aes(label = name), vjust = 0, nudge_y = 1
+  geom_text(data = tsne_data %>% 
+              group_by(hierarchical_cluster) %>% 
+              filter(X == max(X) | X == min(X) | Y == max(Y) | Y == min(Y)) %>% 
+              arrange(hierarchical_cluster), 
+            aes(label = name), vjust = 0, nudge_y = 1
   ) +
   scale_color_brewer(palette = "Dark2") +
-  theme_bw()
+  theme_bw() +
+  labs(title = "t-SNE Visualization with Hierarchical Cluster Labels")  
 
 # Perform DBSCAN clustering
 dbscan_fit <- dbscan(gower_dist, eps = 0.1, minPts = 5)  
@@ -167,22 +165,23 @@ tsne_data %>%
   geom_bar(stat = 'Identity') +  
   coord_flip() +  
   scale_fill_brewer(palette = "Dark2") +
-  theme_bw()
+  theme_bw() +
+  labs(title = "Distribution of DBSCAN Clusters")  
 
 # Plot the t-SNE visualization with DBSCAN cluster labels
 ggplot(aes(x = X, y = Y), data = tsne_data) +
   geom_point(aes(color = dbscan_cluster)) +
   theme(legend.position = "none") + 
-  geom_text( 
-    data = tsne_data %>% 
-      filter(!dbscan_noise) %>%  # Exclude noise points
-      group_by(dbscan_cluster) %>% 
-      filter(X == max(X) | X == min(X) | Y == max(Y) | Y == min(Y)) %>% 
-      arrange(dbscan_cluster), 
-    aes(label = name), vjust = 0, nudge_y = 1
+  geom_text(data = tsne_data %>% 
+              filter(!dbscan_noise) %>%  # Exclude noise points
+              group_by(dbscan_cluster) %>% 
+              filter(X == max(X) | X == min(X) | Y == max(Y) | Y == min(Y)) %>% 
+              arrange(dbscan_cluster), 
+            aes(label = name), vjust = 0, nudge_y = 1
   ) +
   scale_color_brewer(palette = "Dark2") +
-  theme_bw()
+  theme_bw() +
+  labs(title = "t-SNE Visualization with DBSCAN Cluster Labels")  
 
 # Perform K-means clustering
 kmeans_fit <- kmeans(gower_dist, centers = 3)
@@ -201,20 +200,22 @@ tsne_data %>%
   geom_bar(stat = 'Identity') +  
   coord_flip() +  
   scale_fill_brewer(palette = "Dark2") +
-  theme_bw()
+  theme_bw() +
+  labs(title = "Distribution of K-means Clusters")  
 
 # Plot the t-SNE visualization with K-means cluster labels
 ggplot(aes(x = X, y = Y), data = tsne_data) +
   geom_point(aes(color = kmeans_cluster)) +
   theme(legend.position = "none") + 
-  geom_text( 
-    data = tsne_data %>% 
-      group_by(kmeans_cluster) %>% 
-      filter(X == max(X) | X == min(X) | Y == max(Y) | Y == min(Y)) %>% 
-      arrange(kmeans_cluster), 
-    aes(label = name), vjust = 0, nudge_y = 1
+  geom_text(data = tsne_data %>% 
+              group_by(kmeans_cluster) %>% 
+              filter(X == max(X) | X == min(X) | Y == max(Y) | Y == min(Y)) %>% 
+              arrange(kmeans_cluster), 
+            aes(label = name), vjust = 0, nudge_y = 1
   ) +
   scale_color_brewer(palette = "Dark2") +
-  theme_bw()
+  theme_bw() +
+  labs(title = "t-SNE Visualization with K-means Cluster Labels")  
+
 
 
